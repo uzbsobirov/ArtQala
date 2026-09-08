fc41e470703fcfa317db91117f1c832ad1d1ac35
Anvar <asobirov9083@gmail.com>
2026-09-08 17:49:22 +0500

    fix: redirect /auth/signin and /auth/signup to /signin and /signup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/auth/signin',
+        destination: '/signin',
+        permanent: true,
+      },
+      {
+        source: '/auth/signup',
+        destination: '/signup',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
